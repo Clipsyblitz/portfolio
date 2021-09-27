9a2d2a0686a2a747514e4a8ac8c2972ab0f95e42
Malone <malone.julienne@gmail.com>
2021-09-27 22:46:02 +0200

    [wip] contact page

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <div class="flex justify-center ">
-	<div class="flex flex-col justify-center items-center mt-20 bg-gray-100 w-4/12 rounded-3xl p-8">
+	<div class="flex flex-col justify-center items-center mt-20 w-4/12 rounded-3xl p-8">
 		<div class=" flex md:text-4xl mb-8 border-b-4">
 			<p>Contact me</p>
 		</div>
@@ -18,24 +18,24 @@
 			<form>
 				<div class="flex flex-col mb-4">
 					<label class="mb-2" for="name">Name</label>
-					<input class="rounded-2xl" id="name" bind:value={form.name} />
+					<input class="shadow-md bg-gray-50 rounded-2xl" id="name" bind:value={form.name} />
 				</div>
 				<div class="flex flex-col mb-4">
 					<label for="surname">Surname</label>
-					<input class="rounded-2xl" id="surname" bind:value={form.surname} />
+					<input class="shadow-md bg-gray-50 rounded-2xl" id="surname" bind:value={form.surname} />
 				</div>
 
 				<div class="flex flex-col mb-4">
 					<label class="mb-2" for="mail">Mail</label>
-					<input class="rounded-2xl" id="mail" bind:value={form.mail} />
+					<input class="shadow-md bg-gray-50 rounded-2xl" id="mail" bind:value={form.mail} />
 				</div>
 				<div class="flex flex-col mb-4">
 					<label class="mb-2" for="subject">Subject</label>
-					<input class="rounded-2xl" id="subject" bind:value={form.subject} />
+					<input class="shadow-md bg-gray-50 rounded-2xl" id="subject" bind:value={form.subject} />
 				</div>
 				<div class="flex flex-col">
 					<label class="mb-2" for="content">Message</label>
-					<input class="rounded-2xl" id="content" bind:value={form.message} />
+					<textarea class="shadow-md bg-gray-50 rounded-2xl" id="content" bind:value={form.message} />
 				</div>
 			</form>
 		</div>
