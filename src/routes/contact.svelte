5763d505d9bcea4c6f64bd052e2ebb753b103683
Malone <malone.julienne@gmail.com>
2021-09-27 22:40:34 +0200

    [wip] contact page

---
+++ src/routes/contact.svelte
@@ -0,0 +1,43 @@
+<script>
+	let form = {
+		name: '',
+		surname: '',
+		mail: '',
+		subject: '',
+		message: ''
+	};
+</script>
+
+<div class="flex justify-center ">
+	<div class="flex flex-col justify-center items-center mt-20 bg-gray-100 w-4/12 rounded-3xl p-8">
+		<div class=" flex md:text-4xl mb-8 border-b-4">
+			<p>Contact me</p>
+		</div>
+
+		<div class="flex flex-col items-center justify-center md:text-3xl">
+			<form>
+				<div class="flex flex-col mb-4">
+					<label class="mb-2" for="name">Name</label>
+					<input class="rounded-2xl" id="name" bind:value={form.name} />
+				</div>
+				<div class="flex flex-col mb-4">
+					<label for="surname">Surname</label>
+					<input class="rounded-2xl" id="surname" bind:value={form.surname} />
+				</div>
+
+				<div class="flex flex-col mb-4">
+					<label class="mb-2" for="mail">Mail</label>
+					<input class="rounded-2xl" id="mail" bind:value={form.mail} />
+				</div>
+				<div class="flex flex-col mb-4">
+					<label class="mb-2" for="subject">Subject</label>
+					<input class="rounded-2xl" id="subject" bind:value={form.subject} />
+				</div>
+				<div class="flex flex-col">
+					<label class="mb-2" for="content">Message</label>
+					<input class="rounded-2xl" id="content" bind:value={form.message} />
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -33,7 +33,7 @@
 		{
 			p: 1,
 			message:
-				"IT has always been a topic of interest to me, as are <b >new technologies</b>. It's boat I know, but I'm really <b >passionate</b> and that's why I give 100% of myself in my work, because I love what I do."
+				"IT has always been a topic of interest to me, as are <b >new technologies</b>. It sounds cliche I know, but I'm really <b >passionate</b> and that's why I give 100% of myself in my work, because I love what I do."
 		},
 		{
 			p: 1,
@@ -51,17 +51,6 @@
 		}
 	];
 </script>
-
-<!-- <div class="text-justify leading-loose md:text-xl">
-	I am currently studying computer engineering in the 4th year at Polytech Grenoble.<br /> I was
-	able to learn to master many technologies such as Java, C, Python or even Angular, ReactJs and SQL
-	during this technical training. <br />IT has always been a topic of interest to me, as are new
-	technologies. It's boat I know, but I'm really passionate and that's why I give 100% of myself in
-	my work, because I love what I do. In other words, code, create and learn. Choosing another
-	specialty now seems impossible to me. <br />I am an ambitious person and that is why in the near
-	future I see myself joining a company that matches my values ​​in which I could learn and improve.
-	Also, in the more or less distant future, I will like to start my own business.
-</div> -->
 
 <div class="mt-20 text-pink-300">
 	<Conversation {messages} />
